7e07d958d38aa779c82c55bd2117a6085e098bb9
slax <slax.10101@gmail.com>
2013-04-13 10:41:31 +0100

    .

--- src/swing/scala/swing/DesktopPane.scala
+++ src/swing/scala/swing/DesktopPane.scala
@@ -5,6 +5,11 @@ import javax.swing.JDesktopPane
 /**
  * User: shlax
  */
+
+object DragMode extends Enumeration{
+  val Live = Value(JDesktopPane.LIVE_DRAG_MODE)
+}
+
 class DesktopPane extends Component with SequentialContainer.Wrapper {
   override lazy val peer: JDesktopPane = new JDesktopPane
 
