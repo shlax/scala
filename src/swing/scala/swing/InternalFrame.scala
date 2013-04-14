924fffebd707a453ebe0f785223051f5504a21f0
slax <slax.10101@gmail.com>
2013-04-14 18:35:37 +0100

    .

--- src/swing/scala/swing/InternalFrame.scala
+++ src/swing/scala/swing/InternalFrame.scala
@@ -18,6 +18,17 @@ class InternalFrame(t:String="",r:Boolean=true,c:Boolean=true,m:Boolean=true,i:B
   def resizable = peer.isResizable
   def resizable_=(a:Boolean) = peer.setResizable(a)
 
+  def closable = peer.isClosable
+  def closable_=(a:Boolean) = peer.setClosable(a)
 
+  def iconable = peer.isIconifiable
+  def iconable_=(a:Boolean) = peer.setIconifiable(a)
 
+  def maximizable = peer.isMaximizable
+  def maximizable_=(a:Boolean) = peer.setMaximizable(a)
+
+  def maximum = peer.isMaximum()
+  def maximum_=(a:Boolean) = peer.setMaximum(a)
+
+  def close() = peer.setClosed(true)
 }
